00d45ccea5ad99f98fefc2f60245064889ea3b9a
Vaka217 <gonzalocabaleiro12@gmail.com>
2023-07-25 13:01:12 +0000

    Task 0 to 7

--- math/linear_algebra/5-across_the_planes.py
+++ math/linear_algebra/5-across_the_planes.py
@@ -6,5 +6,5 @@ def add_matrices2D(mat1, mat2):
     if matrix_shape(mat1) != matrix_shape(mat2):
         return None
 
-    arr = [[mat1[i][j] + mat2[i][j] for i in range(len(mat1))] for j in range(len(mat1[0]))]
+    arr = [[mat1[i][j] + mat2[i][j] for j in range(len(mat1[0]))] for i in range(len(mat1))]
     return arr
