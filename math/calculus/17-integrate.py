dcd63d588fd2cb4e8b9ec0fccabe20b0efd134c6
Vaka217 <gonzalocabaleiro12@gmail.com>
2023-08-03 16:55:51 +0000

    Add case in 17

--- math/calculus/17-integrate.py
+++ math/calculus/17-integrate.py
@@ -6,6 +6,8 @@ def poly_integral(poly, C=0):
     """calculates the integral of a polynomial"""
     if not isinstance(poly, list) or len(poly) < 1 or not isinstance(C, int):
         return None
+    if len(poly) == 1:
+        return [C, 0]
 
     return [C] + [int(poly[x] / (x + 1)) if poly[x] / (x + 1)
                   == int(poly[x] / (x + 1)) else poly[x] / (x + 1)
