2e40c978f47a14fbc81b925f5fc645c518c62491
Vaka217 <gonzalocabaleiro12@gmail.com>
2023-08-16 15:02:38 +0000

    Add conditional for positive integers in layers

--- supervised_learning/classification/17-deep_neural_network.py
+++ supervised_learning/classification/17-deep_neural_network.py
@@ -13,6 +13,8 @@ class DeepNeuralNetwork():
             raise ValueError("nx must be a positive integer")
         if not isinstance(layers, list) or len(layers) == 0:
             raise TypeError("layers must be a list of positive integers")
+        if not all([layer == int(layer) and layer > 0 for layer in layers]):
+            raise TypeError("layers must be a list of positive integers")
 
         self.__L = len(layers)
         self.__cache = {}
