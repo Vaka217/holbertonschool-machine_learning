0198b5fed801e62f56b74001258bac7d9b60e5e4
Vaka217 <gonzalocabaleiro12@gmail.com>
2023-08-16 16:17:10 +0000

    One-hot tasks

--- supervised_learning/classification/24-one_hot_encode.py
+++ supervised_learning/classification/24-one_hot_encode.py
@@ -5,4 +5,8 @@ import numpy as np
 
 def one_hot_encode(Y, classes):
     """Converts a numeric label vector into a one-hot matrix"""
-    return np.array([[1 if y == i else 0 for y in Y] for i in range(classes)])
+    if not isinstance(Y, np.ndarray) or not isinstance(
+            classes, int) or classes < 2 or classes < np.max(Y):
+        return None
+    return np.array([[1. if y == i else 0. for y in Y] for i in range(
+        classes)]).T
---
+++ supervised_learning/classification/25-one_hot_decode.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+"""One-Hot Decode Module"""
+import numpy as np
+
+
+def one_hot_decode(one_hot):
+    """Converts a numeric label vector into a one-hot matrix"""
+    if not isinstance(one_hot, np.ndarray) or not one_hot:
+        return None
+    return np.array([np.argmax(column) for column in one_hot])
