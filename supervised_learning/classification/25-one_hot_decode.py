b75a475dc2ade60499fa94c63eda7268d5619c28
Vaka217 <gonzalocabaleiro12@gmail.com>
2023-08-22 16:02:52 +0000

    Task 25

--- supervised_learning/classification/25-one_hot_decode.py
+++ supervised_learning/classification/25-one_hot_decode.py
@@ -5,6 +5,6 @@ import numpy as np
 
 def one_hot_decode(one_hot):
     """Converts one-hot matrix into a vector of labels"""
-    if not isinstance(one_hot, np.ndarray):
+    if not isinstance(one_hot, np.ndarray) or one_hot.ndim != 2:
         return None
     return np.array([np.argmax(column) for column in one_hot])
