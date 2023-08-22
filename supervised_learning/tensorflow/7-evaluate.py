c89268ea5c4ba8ef2f3337e526c7665a3cb0227a
Vaka217 <gonzalocabaleiro12@gmail.com>
2023-08-22 19:33:28 +0000

    Fix collections names

--- supervised_learning/tensorflow/7-evaluate.py
+++ supervised_learning/tensorflow/7-evaluate.py
@@ -8,11 +8,11 @@ def evaluate(X, Y, save_path):
     with tf.Session() as sess:
         new_saver = tf.train.import_meta_graph(save_path + ".meta")
         new_saver.restore(sess, tf.train.latest_checkpoint('./'))
-        y_pred = tf.get_collection("y_pred")[0]
-        loss = tf.get_collection("loss")[1]
-        accuracy = tf.get_collection("accuracy")[2]
-        x = tf.get_collection("x")[0]
-        y = tf.get_collection("y")[1]
+        y_pred = tf.get_collection("tensors")[0]
+        loss = tf.get_collection("tensors")[1]
+        accuracy = tf.get_collection("tensors")[2]
+        x = tf.get_collection("placeholders")[0]
+        y = tf.get_collection("placeholders")[1]
         y_pred, accuracy, loss = sess.run([y_pred, accuracy, loss],
                                           feed_dict={x: X, y: Y})
     return y_pred, accuracy, loss
