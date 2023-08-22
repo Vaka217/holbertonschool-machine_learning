47c6bdd3f265c879f1600992cc5636428bc67f8c
Vaka217 <gonzalocabaleiro12@gmail.com>
2023-08-22 19:29:53 +0000

    Task 7

--- supervised_learning/tensorflow/7-evaluate.py
+++ supervised_learning/tensorflow/7-evaluate.py
@@ -6,13 +6,13 @@ import tensorflow as tf
 def evaluate(X, Y, save_path):
     """Evaluates the output of a neural network"""
     with tf.Session() as sess:
-        new_saver = tf.train.import_meta_graph("./{}".format(save_path))
-        new_saver.restore(sess, "./{}".format(save_path))
-        y_pred = tf.get_collection("y_pred")
-        loss = tf.get_collection("loss")
-        accuracy = tf.get_collection("accuracy")
-        x = tf.get_collection("x")
-        y = tf.get_collection("y")
+        new_saver = tf.train.import_meta_graph(save_path + ".meta")
+        new_saver.restore(sess, tf.train.latest_checkpoint('./'))
+        y_pred = tf.get_collection("y_pred")[0]
+        loss = tf.get_collection("loss")[1]
+        accuracy = tf.get_collection("accuracy")[2]
+        x = tf.get_collection("x")[0]
+        y = tf.get_collection("y")[1]
         y_pred, accuracy, loss = sess.run([y_pred, accuracy, loss],
                                           feed_dict={x: X, y: Y})
     return y_pred, accuracy, loss
