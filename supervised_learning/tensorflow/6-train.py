6062833d042f0e6516c738009ac99f5f2ad7b493
Vaka217 <gonzalocabaleiro12@gmail.com>
2023-08-22 17:47:04 +0000

    Fix indentation

--- supervised_learning/tensorflow/6-train.py
+++ supervised_learning/tensorflow/6-train.py
@@ -44,8 +44,8 @@ def train(X_train, Y_train, X_valid, Y_valid, layer_sizes, activations,
                 print("\tValidation Cost: {}".format(valid_loss))
                 print("\tValidation Accuracy: {}".format(valid_accuracy))
 
-                if i < iterations:
-                    sess.run(train_op, feed_dict={x: X_train, y: Y_train})
+            if i < iterations:
+                sess.run(train_op, feed_dict={x: X_train, y: Y_train})
         saver = tf.train.Saver()
         saver.save(sess, save_path)
     return save_path
