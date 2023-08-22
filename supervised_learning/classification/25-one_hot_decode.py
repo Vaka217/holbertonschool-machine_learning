3818c38578435f4c9f547df48557f284e3a12b62
Vaka217 <gonzalocabaleiro12@gmail.com>
2023-08-22 16:07:20 +0000

    Transpose one_hot to decode

--- supervised_learning/classification/25-one_hot_decode.py
+++ supervised_learning/classification/25-one_hot_decode.py
@@ -7,4 +7,4 @@ def one_hot_decode(one_hot):
     """Converts one-hot matrix into a vector of labels"""
     if not isinstance(one_hot, np.ndarray) or one_hot.ndim != 2:
         return None
-    return np.array([np.argmax(column) for column in one_hot])
+    return np.array([np.argmax(column) for column in one_hot.T])
