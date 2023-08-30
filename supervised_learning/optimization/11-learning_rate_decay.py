e9bf50c8a13acfc3f5a0b41125418e65ba12034c
Vaka217 <gonzalocabaleiro12@gmail.com>
2023-08-30 20:02:30 +0000

    Change name from 12 copy

--- supervised_learning/optimization/11-learning_rate_decay.py
+++ supervised_learning/optimization/11-learning_rate_decay.py
@@ -5,4 +5,6 @@ import numpy as np
 
 def learning_rate_decay(alpha, decay_rate, global_step, decay_step):
     """Updates the learning rate using inverse time decay using numpy"""
+    if global_step / decay_rate != 0:
+        return alpha
     return alpha / (1 + decay_rate * global_step / decay_step)
