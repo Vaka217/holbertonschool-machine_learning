7fd6734e8236290f05613a2d6ac41ef60bdca23e
Vaka217 <gonzalocabaleiro12@gmail.com>
2023-08-30 19:20:24 +0000

    Change Adam typo

--- supervised_learning/optimization/10-Adam.py
+++ supervised_learning/optimization/10-Adam.py
@@ -7,6 +7,6 @@ def create_Adam_op(loss, alpha, beta1, beta2, epsilon):
     """Creates the training operation for a neural network in tensorflow
     using the Adam optimization algorithm"""
 
-    optimizer = tf.train.RMSPropOptimizer(alpha, beta1, beta2, epsilon)
+    optimizer = tf.train.AdamOptimizer(alpha, beta1, beta2, epsilon)
     train = optimizer.minimize(loss)
     return train
