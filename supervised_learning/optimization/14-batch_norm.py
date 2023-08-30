eb9f49e844810f70f71149bd4338093b07c6ee63
Vaka217 <gonzalocabaleiro12@gmail.com>
2023-08-30 21:10:11 +0000

    fix pycode 14

--- supervised_learning/optimization/14-batch_norm.py
+++ supervised_learning/optimization/14-batch_norm.py
@@ -8,7 +8,8 @@ def create_batch_norm_layer(prev, n, activation):
     tensorflow"""
     heetal = tf.keras.initializers.VarianceScaling(mode='fan_avg')
 
-    layer = tf.keras.layers.Dense(n, activation=None, kernel_initializer=heetal)
+    layer = tf.keras.layers.Dense(n, activation=None,
+                                  kernel_initializer=heetal)
     Z = layer(prev)
 
     beta = tf.Variable(initial_value=tf.zeros(shape=[n]), name="beta")
@@ -16,7 +17,8 @@ def create_batch_norm_layer(prev, n, activation):
 
     m, v = tf.nn.moments(Z, axes=[0])
 
-    batch_norm = tf.nn.batch_normalization(Z, mean=m, variance=v, offset=beta, scale=gamma, variance_epsilon=1e-8)
+    batch_norm = tf.nn.batch_normalization(Z, mean=m, variance=v, offset=beta,
+                                           scale=gamma, variance_epsilon=1e-8)
 
     if activation is not None:
         batch_norm = activation(batch_norm)
