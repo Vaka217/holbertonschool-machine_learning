4d8950bdd5b6416fd7998194ec8278154e47c111
Vaka217 <gonzalocabaleiro12@gmail.com>
2023-08-30 19:11:15 +0000

    Change square root

--- supervised_learning/optimization/8-RMSProp.py
+++ supervised_learning/optimization/8-RMSProp.py
@@ -7,6 +7,6 @@ def create_RMSProp_op(loss, alpha, beta2, epsilon):
     """Creates the training operation for a neural network in tensorflow
     using the RMSProp optimization algorithm"""
 
-    optimizer = tf.train.RMSPropOptimizer(alpha, beta2, epsilon)
+    optimizer = tf.train.RMSPropOptimizer(alpha, beta2, epsilon=epsilon)
     train = optimizer.minimize(loss)
     return train
--- supervised_learning/optimization/9-Adam.py
+++ supervised_learning/optimization/9-Adam.py
@@ -1,6 +1,5 @@
 #!/usr/bin/env python3
 """Variable with Adam"""
-import numpy as np
 
 
 def update_variables_Adam(alpha, beta1, beta2, epsilon, var, grad, v, s, t):
@@ -8,5 +7,5 @@ def update_variables_Adam(alpha, beta1, beta2, epsilon, var, grad, v, s, t):
 
     v = (beta1 * v + (1 - beta1) * grad) / (1 - (beta1 ** t))
     s = (beta2 * s + (1 - beta2) * (grad ** 2)) / (1 - (beta2 ** t))
-    var -= alpha * (v / (np.sqrt(s) + epsilon))
+    var -= alpha * v / (s ** 0.5 + epsilon)
     return var, v, s
