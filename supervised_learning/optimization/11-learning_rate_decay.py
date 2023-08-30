95c853b32fbeef1892628428c6bfe12b2219e9e9
Vaka217 <gonzalocabaleiro12@gmail.com>
2023-08-30 20:18:51 +0000

    Add parenthesis

--- supervised_learning/optimization/11-learning_rate_decay.py
+++ supervised_learning/optimization/11-learning_rate_decay.py
@@ -5,4 +5,4 @@ import numpy as np
 
 def learning_rate_decay(alpha, decay_rate, global_step, decay_step):
     """Updates the learning rate using inverse time decay using numpy"""
-    return alpha / (1 + decay_rate * global_step // decay_step)
+    return alpha / (1 + decay_rate * (global_step // decay_step))
