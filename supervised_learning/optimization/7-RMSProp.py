e87f7b55badb8101ea44b341c92c30e795ededcc
Vaka217 <gonzalocabaleiro12@gmail.com>
2023-08-30 18:55:00 +0000

    Remove numpy

--- supervised_learning/optimization/7-RMSProp.py
+++ supervised_learning/optimization/7-RMSProp.py
@@ -1,12 +1,10 @@
 #!/usr/bin/env python3
 """Variable with RMSProp"""
-import tensorflow.compat.v1 as tf
-import numpy as np
 
 
 def update_variables_RMSProp(alpha, beta2, epsilon, var, grad, s):
     """Updates a variable using the RMSProp optimization algorithm"""
 
     s = beta2 * s + (1 - beta2) * (grad ** 2)
-    var -= alpha * (grad / (np.sqrt(s) + epsilon))
+    var -= alpha * (grad / (s ** 1/2 + epsilon))
     return var, s
