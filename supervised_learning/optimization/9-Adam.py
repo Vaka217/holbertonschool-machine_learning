4238ac9b7487380c104be42570ff2eb1c9941de3
Vaka217 <gonzalocabaleiro12@gmail.com>
2023-08-30 19:47:23 +0000

    Return first/second moment without bias correction

--- supervised_learning/optimization/9-Adam.py
+++ supervised_learning/optimization/9-Adam.py
@@ -5,7 +5,9 @@
 def update_variables_Adam(alpha, beta1, beta2, epsilon, var, grad, v, s, t):
     """Updates a variable using Adam optimization algorithm"""
 
-    v = (beta1 * v + (1 - beta1) * grad) / (1 - (beta1 ** t))
-    s = (beta2 * s + (1 - beta2) * (grad ** 2)) / (1 - (beta2 ** t))
-    var -= alpha * v / (s ** 0.5 + epsilon)
+    v = (beta1 * v + (1 - beta1) * grad)
+    s = (beta2 * s + (1 - beta2) * (grad ** 2))
+    v_corrected = v / (1 - beta1 ** t)
+    s_corrected = s / (1 - beta2 ** t)
+    var -= alpha * v_corrected / (s_corrected ** 0.5 + epsilon)
     return var, v, s
