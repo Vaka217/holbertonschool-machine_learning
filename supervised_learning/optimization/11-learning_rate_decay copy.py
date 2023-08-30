4d07570d8d5b3936e38d18fd8a966851dfce28ea
Vaka217 <gonzalocabaleiro12@gmail.com>
2023-08-30 20:00:23 +0000

    Task 11

---
+++ supervised_learning/optimization/11-learning_rate_decay copy.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+"""Learning Rate Decay"""
+import numpy as np
+
+
+def learning_rate_decay(alpha, decay_rate, global_step, decay_step):
+    """Updates the learning rate using inverse time decay using numpy"""
+    return alpha / (1 + decay_rate * global_step / decay_step)
