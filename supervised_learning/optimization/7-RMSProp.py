a8dce2ba210e5e5b2464f1f8c0adb3f7a9fcb0c9
Vaka217 <gonzalocabaleiro12@gmail.com>
2023-08-30 18:57:12 +0000

    Add parenthesis

--- supervised_learning/optimization/7-RMSProp.py
+++ supervised_learning/optimization/7-RMSProp.py
@@ -6,5 +6,5 @@ def update_variables_RMSProp(alpha, beta2, epsilon, var, grad, s):
     """Updates a variable using the RMSProp optimization algorithm"""
 
     s = beta2 * s + (1 - beta2) * (grad ** 2)
-    var -= alpha * (grad / (s ** 1/2 + epsilon))
+    var -= alpha * (grad / ((s ** 1/2) + epsilon))
     return var, s
