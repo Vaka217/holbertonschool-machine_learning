f2b4f9da740d3998af711c3ee6883774fc68d7ba
Vaka217 <gonzalocabaleiro12@gmail.com>
2023-09-11 14:43:53 -0300

    Task 0

---
+++ supervised_learning/keras/0-sequential.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+"""Builds a neural network with the Keras library"""
+import tensorflow.keras as K
+
+
+def build_model(nx, layers, activations, lambtha, keep_prob):
+    """Builds a neural network with the Keras library
+
+    Args:
+        nx (int): number of input features to the network
+        layers (list): list containing the number of nodes in each layer of
+        the network
+        activations (list): list containing the activation functions used for
+        each layer of the network
+        lambtha (float): L2 regularization parameter
+        keep_prob (float): probability that a node will be kept for dropout
+
+    Returns:
+        model: the keras model
+    """
+
+    model = K.Sequential()
+    for i in range(len(layers) - 1):
+        model.add(K.layers.Dropout(keep_prob, input_shape=(nx,)))
+        model.add(K.layers.Dense(layers[i], activation=activations[i],
+                                 kernel_regularizer=K.regularizers.L2(lambtha),
+                                 input_shape=(nx,)))
+    model.add(K.layers.Dense(layers[i + 1], activation=activations[i + 1],
+                             kernel_regularizer=K.regularizers.L2(lambtha),
+                             input_shape=(nx,)))
+    return model
