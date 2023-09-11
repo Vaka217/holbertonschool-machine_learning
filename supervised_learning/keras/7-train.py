71271666b12b90a512c9e24a765d170d39c6b207
Vaka217 <gonzalocabaleiro12@gmail.com>
2023-09-11 17:45:44 -0300

    Task 6 to 8

--- supervised_learning/keras/7-train.py
+++ supervised_learning/keras/7-train.py
@@ -5,9 +5,10 @@ import tensorflow.keras as K
 
 def train_model(network, data, labels, batch_size, epochs,
                 validation_data=None, early_stopping=False, patience=0,
+                learning_rate_decay=False, alpha=0.1, decay_rate=1,
                 verbose=True, shuffle=False):
     """Trains a model using mini-batch gradient descent, analyze validation
-    data and uses early stopping
+    data, uses early stopping and learning rate decay.
 
     Args:
         network (Model): the model to train.
@@ -23,6 +24,10 @@ def train_model(network, data, labels, batch_size, epochs,
         should be used. Defaults to False.
         patience (int, optional): the patience used for early stopping.
         Defaults to 0.
+        learning_rate_decay (bool, optional): indicates whether learning rate.
+        decay should be used. Defaults to False.
+        alpha (float, optional): the initial learning rate. Defaults to 0.1.
+        decay_rate (int, optional): the decay rate. Defaults to 1.
         verbose (bool, optional): boolean that determines if output should be
         printed during training. Defaults to True.
         shuffle (bool, optional): boolean that determines whether to shuffle
@@ -33,8 +38,18 @@ def train_model(network, data, labels, batch_size, epochs,
         History: History object generated after training the model
     """
 
-    callback = K.callbacks.EarlyStopping(
+    if learning_rate_decay and validation_data:
+        def lr_schedule(epoch):
+            return alpha / (1 + decay_rate * epoch)
+
+        learning_rate_decay = K.callbacks.LearningRateScheduler(lr_schedule,
+                                                                verbose=True)
+
+    early_stopping = K.callbacks.EarlyStopping(
         patience=patience) if early_stopping and validation_data else None
+    
+    callback = [early_stopping, learning_rate_decay]
+
     return network.fit(data, labels, batch_size, epochs, verbose,
                        shuffle=shuffle, validation_data=validation_data,
                        callbacks=callback)
