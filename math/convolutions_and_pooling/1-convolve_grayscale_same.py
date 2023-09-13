6bdd1f26f78a71f2146e390352d8acdaa9fb2aa4
Vaka217 <gonzalocabaleiro12@gmail.com>
2023-09-13 17:19:11 +0000

    change ceil for max

--- math/convolutions_and_pooling/1-convolve_grayscale_same.py
+++ math/convolutions_and_pooling/1-convolve_grayscale_same.py
@@ -24,8 +24,8 @@ def convolve_grayscale_same(images, kernel):
 
     kh, kw = kernel.shape
 
-    ph = max(kh // 2 - 1, kh // 2)
-    pw = max(kw // 2 - 1, kw // 2)
+    ph = max((kh - 1) // 2, kh // 2)
+    pw = max((kw - 1) // 2, kw // 2)
 
     images = np.pad(images, [(0, 0), (ph, ph), (pw, pw)])
     m, h, w = images.shape
