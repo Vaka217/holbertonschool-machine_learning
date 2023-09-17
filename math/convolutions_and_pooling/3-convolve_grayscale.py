65054c47c909b9a6b13bb88f9d9a2c1c1e8725cd
Vaka217 <gonzalocabaleiro12@gmail.com>
2023-09-17 15:05:12 -0300

    Task 3, change conditional valid for same

--- math/convolutions_and_pooling/3-convolve_grayscale.py
+++ math/convolutions_and_pooling/3-convolve_grayscale.py
@@ -37,18 +37,17 @@ def convolve_grayscale(images, kernel, padding='same', stride=(1, 1)):
 
     if isinstance(padding, tuple):
         ph, pw = padding
-    elif padding == 'valid':
+    elif padding == 'same':
         ph = (((h - 1) * sh + kh - h) // 2) + 1
         pw = (((w - 1) * sw + kw - w) // 2) + 1
 
-    if padding != 'same':
+    if pw and ph:
         images = np.pad(images, [(0, 0), (ph, ph), (pw, pw)])
 
     outh = ((h + (2 * ph) - kh) // sh) + 1
     outw = ((w + (2 * pw) - kw) // sw) + 1
 
     output = np.zeros((m, outh, outw))
-    print(output.shape)
 
     for i in range(outh):
         for j in range(outw):
