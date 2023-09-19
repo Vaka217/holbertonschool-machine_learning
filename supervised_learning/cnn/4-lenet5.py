Task 4 fix softmax

supervised_learning/cnn/4-lenet5.py
@@ -22,10 +22,12 @@ def lenet5(x, y):
     """
     he_normal = tf.keras.initializers.VarianceScaling(scale=2.0)
 
-    conv_1 = tf.layers.conv2d(x, 6, 5, padding="same", activation=tf.nn.relu)
+    conv_1 = tf.layers.conv2d(x, 6, 5, padding="same", activation=tf.nn.relu,
+                              kernel_initializer=he_normal)
     max_pool_1 = tf.layers.max_pooling2d(conv_1, 2, 2)
     conv_2 = tf.layers.conv2d(max_pool_1, 16, 5, padding="valid",
-                              activation=tf.nn.relu)
+                              activation=tf.nn.relu,
+                              kernel_initializer=he_normal)
     max_pool_2 = tf.layers.max_pooling2d(conv_2, 2, 2)
     flatten = tf.layers.flatten(max_pool_2)
     full_connected_1 = tf.layers.dense(flatten, 120, activation=tf.nn.relu,
@@ -33,16 +35,17 @@ def lenet5(x, y):
     full_connected_2 = tf.layers.dense(full_connected_1, 84,
                                        activation=tf.nn.relu,
                                        kernel_initializer=he_normal)
-    y_pred = tf.layers.dense(full_connected_2, 10, activation=tf.math.softmax,
+    y_pred = tf.layers.dense(full_connected_2, 10,
                              kernel_initializer=he_normal)
+    output = tf.nn.softmax(y_pred)
 
     loss = tf.losses.softmax_cross_entropy(y, y_pred)
 
     optimizer = tf.train.AdamOptimizer()
     train = optimizer.minimize(loss)
 
-    pred_labels = tf.argmax(y_pred, axis=1)
+    pred_labels = tf.argmax(output, axis=1)
     correct_pred = tf.equal(pred_labels, tf.argmax(y, axis=1))
     accuracy = tf.reduce_mean(tf.cast(correct_pred, tf.float32))
 
-    return y_pred, train, loss, accuracy
+    return output, train, loss, accuracy