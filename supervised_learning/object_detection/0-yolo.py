700bc38c0dd4ff23280d1257b239c032fb0b39c2
Vaka217 <gonzalocabaleiro12@gmail.com>
2023-10-10 17:11:40 +0000

    Fix classes init

--- supervised_learning/object_detection/0-yolo.py
+++ supervised_learning/object_detection/0-yolo.py
@@ -11,7 +11,7 @@ class Yolo():
     def __init__(self, model_path, classes_path, class_t, nms_t, anchors):
         self.model = K.models.load_model(model_path)
         with open(classes_path, 'r', encoding='utf-8') as f:
-            self.class_names = [line for line in f]
+            self.class_names = [line.strip() for line in f]
         self.class_t = class_t
         self.nms_t = nms_t
         self.anchors = anchors
