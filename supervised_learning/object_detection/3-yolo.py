Fix iou calculation

supervised_learning/object_detection/3-yolo.py
@@ -73,8 +73,8 @@ class Yolo():
 
             b_x = (1 / (1 + np.exp(-t_x)) + c_x) / grid_width
             b_y = (1 / (1 + np.exp(-t_y)) + c_y) / grid_height
-            b_w = (np.exp(t_w) * p_w) / self.model.input.shape[1]
-            b_h = (np.exp(t_h) * p_h) / self.model.input.shape[2]
+            b_w = (np.exp(t_w) * p_w) / self.model.input.shape[1].value
+            b_h = (np.exp(t_h) * p_h) / self.model.input.shape[2].value
 
             box = np.empty((grid_height, grid_width, anchors_size, 4))
 
@@ -151,10 +151,10 @@ class Yolo():
             """
             xi_1 = max(box_1[0], box_2[0])
             yi_1 = max(box_1[1], box_2[1])
-            xi_2 = max(box_1[2], box_2[2])
-            yi_2 = max(box_1[3], box_2[3])
+            xi_2 = min(box_1[2], box_2[2])
+            yi_2 = min(box_1[3], box_2[3])
 
-            intersection = (xi_2 - xi_1) * (yi_2 - yi_1)
+            intersection = max(0, yi_2 - yi_1 + 1) * max(0, xi_2 - xi_1 + 1)
 
             box_1_area = (box_1[3] - box_1[1]) * (box_1[2] - box_1[0])
             box_2_area = (box_2[3] - box_2[1]) * (box_2[2] - box_2[0])
@@ -187,14 +187,23 @@ class Yolo():
         box_predictions = []
         predicted_box_classes = []
         predicted_box_scores = []
+        better_iou_boxes = []
         for i, filtered_boxes_class in enumerate(filtered_boxes):
             for j, filtered_box in enumerate(filtered_boxes_class):
-                iou = intersection_over_union(best_filtered_boxes[i],
-                                              filtered_box)
-                if iou <= self.nms_t:
+                if better_iou_boxes:
+                    for better_iou_box in better_iou_boxes:
+                        iou = intersection_over_union(better_iou_box,
+                                                      filtered_box)
+                else:
+                    iou = intersection_over_union(best_filtered_boxes[i],
+                                                  filtered_box)
+                if iou <= self.nms_t or np.array_equal(filtered_box,
+                                                       best_filtered_boxes[i]):
                     box_predictions.append(filtered_box)
                     predicted_box_classes.append(box_classes[i])
                     predicted_box_scores.append(box_scores[i][j])
+                    better_iou_boxes.append(filtered_box)
+            better_iou_boxes = []
 
         return np.array(box_predictions), np.array(
             predicted_box_classes), np.array(predicted_box_scores)