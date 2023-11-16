a6cd07797904daea22cd5a84b98f9bd24065ddd9
Vaka217 <gonzalocabaleiro12@gmail.com>
2023-11-16 13:16:41 -0300

    Task 2 Calculate total intra-cluster variance for a data set

---
+++ unsupervised_learning/clustering/2-variance.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+"""Variance Module"""
+import numpy as np
+
+
+def variance(X, C):
+    """Calculates the total intra-cluster variance for a data set:
+
+    X is a numpy.ndarray of shape (n, d) containing the data set
+    C is a numpy.ndarray of shape (k, d) containing the centroid means for
+    each cluster
+
+    Returns: var, or None on failure
+    var is the total variance
+    """
+
+    distances = np.linalg.norm(X[:, np.newaxis, :] - C, axis=-1)
+
+    clss = np.argmin(distances, axis=1)
+
+    cluster_distances = distances[np.arange(len(X)), clss]
+
+    var = np.sum(cluster_distances ** 2)
+
+    return var
