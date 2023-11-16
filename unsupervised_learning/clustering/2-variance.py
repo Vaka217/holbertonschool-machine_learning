2857fbfb5d572163a83891408135e7071d443835
Vaka217 <gonzalocabaleiro12@gmail.com>
2023-11-16 13:21:25 -0300

    Fix pycodestyle

--- unsupervised_learning/clustering/2-variance.py
+++ unsupervised_learning/clustering/2-variance.py
@@ -17,7 +17,8 @@ def variance(X, C):
     if not isinstance(X, np.ndarray) or len(X.shape) != 2:
         return None
 
-    if not isinstance(C, np.ndarray) or len(C.shape) != 2 or C.shape[1] != X.shape[1]:
+    if not isinstance(C, np.ndarray) or len(C.shape) != 2 or C.shape[1] \
+            != X.shape[1]:
         return None
 
     distances = np.linalg.norm(X[:, np.newaxis, :] - C, axis=-1)
