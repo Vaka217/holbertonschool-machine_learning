54255303e4332bc17f4cd59500aa9cdef47f7722
Vaka217 <gonzalocabaleiro12@gmail.com>
2023-11-19 15:45:59 -0300

    Add expectation step, maximization step, and expectation maximization for a GMM

---
+++ unsupervised_learning/clustering/7-maximization.py
@@ -0,0 +1,37 @@
+import numpy as np
+initialize = __import__('4-initialize').initialize
+
+
+def maximization(X, g):
+    """Calculates the maximization step in the EM algorithm for a GMM:
+
+    X is a numpy.ndarray of shape (n, d) containing the data set
+    g is a numpy.ndarray of shape (k, n) containing the posterior probabilities
+    for each data point in each cluster
+    You may use at most 1 loop
+    Returns: pi, m, S, or None, None, None on failure
+    pi is a numpy.ndarray of shape (k,) containing the updated priors for each
+    cluster
+    m is a numpy.ndarray of shape (k, d) containing the updated centroid means
+    for each cluster
+    S is a numpy.ndarray of shape (k, d, d) containing the updated covariance
+    matrices for each cluster
+    """
+
+    n, d = X.shape
+    k = g.shape[0]
+
+    pi, m, _ = initialize(X, k)
+    S = np.zeros((k, d, d))
+
+    sum_g = np.sum(g, axis=1)
+
+    pi = sum_g / n
+    m = np.dot(g, X) / sum_g[:, np.newaxis]
+
+    for i in range(k):
+        diff = X - m[i]
+        weighted_diff = (g[i, :, np.newaxis] * diff).T
+        S[i] = np.dot(weighted_diff, diff) / sum_g[i]
+
+    return pi, m, S
---
+++ unsupervised_learning/clustering/8-EM.py
@@ -0,0 +1,51 @@
+import numpy as np
+initialize = __import__('4-initialize').initialize
+expectation = __import__('6-expectation').expectation
+maximization = __import__('7-maximization').maximization
+
+
+def expectation_maximization(X, k, iterations=1000, tol=1e-5, verbose=False):
+    """Performs the expectation maximization for a GMM:
+
+    X is a numpy.ndarray of shape (n, d) containing the data set
+    k is a positive integer containing the number of clusters
+    iterations is a positive integer containing the maximum number of
+    iterations for the algorithm
+    tol is a non-negative float containing tolerance of the log likelihood,
+    used to determine early stopping i.e. if the difference is less than or
+    equal to tol you should stop the algorithm
+    verbose is a boolean that determines if you should print information about
+    the algorithm
+    If True, print Log Likelihood after {i} iterations: {l} every 10 iterations
+    and after the last iteration
+    {i} is the number of iterations of the EM algorithm
+    {l} is the log likelihood, rounded to 5 decimal places
+
+    Returns: pi, m, S, g, l, or None, None, None, None, None on failure
+    pi is a numpy.ndarray of shape (k,) containing the priors for each cluster
+    m is a numpy.ndarray of shape (k, d) containing the centroid means for each
+    cluster
+    S is a numpy.ndarray of shape (k, d, d) containing the covariance matrices
+    for each cluster
+    g is a numpy.ndarray of shape (k, n) containing the probabilities for each
+    data point in each cluster
+    l is the log likelihood of the model
+    """
+
+    pi, m, S = initialize(X, k)
+
+    prev_l = None
+
+    for i in range(iterations):
+        g, likelihood = expectation(X, pi, m, S)
+        pi, m, S = maximization(X, g)
+
+        if verbose and (i % 10 == 0 or i == iterations - 1):
+            print(f"Log Likelihood after {i} iterations: {l:.5f}")
+
+        if prev_l is not None and abs(likelihood - prev_l) <= tol:
+            break
+
+        prev_l = likelihood
+
+    return pi, m, S, g, likelihood
