d9e59df352e0302a32036cd3ce319459e76b4855
Vaka217 <gonzalocabaleiro12@gmail.com>
2023-11-20 13:58:06 -0300

    Task 0 and 1 from Hidden Markov Models

---
+++ unsupervised_learning/hmm/0-markov_chain.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+"""Markov Chain Module"""
+import numpy as np
+
+
+def markov_chain(P, s, t=1):
+    """Determines the probability of a markov chain being in a particular
+    state after a specified number of iterations:
+
+    P is a square 2D numpy.ndarray of shape (n, n) representing the transition
+    matrix
+    P[i, j] is the probability of transitioning from state i to state j
+    n is the number of states in the markov chain
+    s is a numpy.ndarray of shape (1, n) representing the probability of
+    starting in each state
+    t is the number of iterations that the markov chain has been through
+
+    Returns: a numpy.ndarray of shape (1, n) representing the probability of
+    being in a specific state after t iterations, or None on failure
+    """
+
+    if not isinstance(P, np.ndarray) or len(P.shape) != 2:
+        return None
+
+    if not isinstance(s, np.ndarray) or len(s.shape) != 2 or \
+            s.shape[1] != P.shape[0]:
+        return None
+
+    return s @ np.linalg.matrix_power(P, t)
---
+++ unsupervised_learning/hmm/1-regular.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+"""Regular Chains Module"""
+import numpy as np
+
+
+def regular(P):
+    """Determines the steady state probabilities of a regular markov chain:
+
+    P is a is a square 2D numpy.ndarray of shape (n, n) representing the
+    transition matrix
+    P[i, j] is the probability of transitioning from state i to state j
+    n is the number of states in the markov chain
+
+    Returns: a numpy.ndarray of shape (1, n) containing the steady state
+    probabilities, or None on failure
+    """
+
+    if not isinstance(P, np.ndarray) or len(P.shape) != 2:
+        return None
+
+    eigen_values, eigen_vectors = np.linalg.eig(P)
+
+    eigen_one_idxs = [round(eigen.real) for eigen in eigen_values].index(1.0)
+
+    return (eigen_vectors[:, eigen_one_idxs]
+            / np.sum(eigen_vectors[:, eigen_one_idxs]))
