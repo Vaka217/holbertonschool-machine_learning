ef6e3da45a8957c36a275659a72ba54886e272f0
Gonzalo Cabaleiro <98929961+Vaka217@users.noreply.github.com>
2023-12-07 00:02:39 -0300

    Update 3-variational.py

--- unsupervised_learning/autoencoders/3-variational.py
+++ unsupervised_learning/autoencoders/3-variational.py
@@ -36,7 +36,7 @@ def autoencoder(input_dims, hidden_layers, latent_dims):
     def sampling(args):
         z_mean, z_log_sigma = args
         epsilon = keras.backend.random_normal(shape=(
-            keras.backend.shape(z_mean)[0], latent_dims), mean=0., stddev=0.1)
+            keras.backend.shape(z_mean)[0], latent_dims), mean=0., stddev=1.0)
         return z_mean + keras.backend.exp(z_log_sigma / 2) * epsilon
 
     latent_space = keras.layers.Lambda(sampling)([mean, log_sigma])
@@ -59,12 +59,12 @@ def autoencoder(input_dims, hidden_layers, latent_dims):
     reconstruction_loss = keras.losses.binary_crossentropy(
         encoder_input, decoder(encoder(encoder_input)[2])) * input_dims
 
-    kl_loss = 1 + log_sigma - \
-        keras.backend.square(mean) - keras.backend.exp(log_sigma)
-    kl_loss = keras.backend.sum(kl_loss, axis=-1)
-    kl_loss *= -0.5
+    kl_loss = -0.5 * keras.backend.mean(
+        1 + z_log_var - keras.backend.square(mean) - keras.backend.exp(
+            log_sigma), axis=-1
+    )
 
-    vae_loss = keras.backend.mean(reconstruction_loss + kl_loss)
+    vae_loss = reconstruction_loss + kl_loss
 
     autoencoder.add_loss(vae_loss)
     autoencoder.compile(optimizer='adam')
