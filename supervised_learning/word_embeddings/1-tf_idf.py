84dcb85fed5e368e05db224c6e6412baae481b5d
Vaka217 <gonzalocabaleiro12@gmail.com>
2023-12-21 16:07:26 -0300

    Use scipy

--- supervised_learning/word_embeddings/1-tf_idf.py
+++ supervised_learning/word_embeddings/1-tf_idf.py
@@ -1,8 +1,6 @@
 #!/usr/bin/env python3
 """TF-IDF Module"""
-import numpy as np
-import math
-import re
+from sklearn.feature_extraction.text import TfidfVectorizer
 
 
 def tf_idf(sentences, vocab=None):
@@ -18,32 +16,7 @@ def tf_idf(sentences, vocab=None):
     f is the number of features analyzed
     features is a list of the features used for embeddings"""
 
-    if vocab is None:
-        vocab = []
-        for sentence in sentences:
-            vocab.extend(re.sub(r"\b\w{1}\b", "", re.sub(
-                r"[^a-zA-Z0-9\s]", " ", sentence.lower())).split())
-        vocab = sorted(list(set(vocab)))
-
-    embeddings = np.zeros((len(sentences), len(vocab)))
-
-    for i, sentence in enumerate(sentences):
-        words = sentence.split()
-        for word in words:
-            word = re.sub(r"\b\w{1}\b", "", re.sub(
-                r"[^a-zA-Z0-9\s]", " ", word.lower())).strip()
-            if word in vocab:
-                embeddings[i][vocab.index(word)] += 1
-        if sum(embeddings[i]) > 0:
-            embeddings[i] /= sum(embeddings[i])
-
-    logs = np.ones((len(embeddings.T)))
-
-    for i, num_words in enumerate(embeddings.T):
-        if np.count_nonzero(num_words) > 0:
-            print(math.log(len(sentences) / np.count_nonzero(num_words), 10))
-            logs[i] = math.log(
-                len(sentences) / np.count_nonzero(num_words), 10)
-            print(logs[i])
-
-    return embeddings * logs, vocab
+    tfids_vector = TfidfVectorizer(vocabulary=vocab)
+    embed = tfids_vector.fit_transform(sentences)
+    features = tfids_vector.get_feature_names_out()
+    return embed.toarray(), features
