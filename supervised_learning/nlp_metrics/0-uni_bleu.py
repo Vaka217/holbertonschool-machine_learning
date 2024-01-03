Fix for modified precision

supervised_learning/nlp_metrics/0-uni_bleu.py
@@ -14,13 +14,7 @@ def uni_bleu(references, sentence):
 
     BP = min(1, np.exp(1 - len(min(references, key=len)) / len(sentence)))
 
-    precision = max([sum(match in reference for match in sentence)
+    precision = max([sum(match in reference for match in set(sentence))
                      for reference in references]) / len(sentence)
-
-    print(references)
-    print(sentence)
-    print(max([sum(match in reference for match in sentence)
-               for reference in references]))
-    print(len(sentence))
 
     return BP * np.exp(np.log(precision))