b6e7012849c6896281dea7f799108edf5a6282a2
Vaka217 <gonzalocabaleiro12@gmail.com>
2024-01-08 17:53:21 -0300

    Fix pycodestyle in task 0

--- supervised_learning/attention/0-rnn_encoder.py
+++ supervised_learning/attention/0-rnn_encoder.py
@@ -30,9 +30,11 @@ class RNNEncoder:
     def __init__(self, vocab, embedding, units, batch):
         self.batch = batch
         self.units = units
-        self.embedding = tf.keras.layers.Embedding(input_dim=vocab, output_dim=embedding)
+        self.embedding = tf.keras.layers.Embedding(input_dim=vocab,
+                                                   output_dim=embedding)
         self.gru = tf.keras.layers.GRU(units, return_sequences=True,
-                                       return_state=True, recurrent_initializer='glorot_uniform')
+                                       return_state=True,
+                                       recurrent_initializer='glorot_uniform')
 
     def initialize_hidden_state(self):
         """Initializes the hidden states for the RNN cell to a tensor of zeros
