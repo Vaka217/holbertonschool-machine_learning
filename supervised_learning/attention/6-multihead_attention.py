c20575d782254ddc025ec46d918d18b89985f373
Vaka217 <gonzalocabaleiro12@gmail.com>
2024-01-09 12:34:44 -0300

    Task 6

---
+++ supervised_learning/attention/6-multihead_attention.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python3
+"""Multi Head Attention Module"""
+import tensorflow as tf
+sdp_attention = __import__('5-sdp_attention').sdp_attention
+
+
+class MultiHeadAttention(tf.keras.layers.Layer):
+    """"""
+
+    def __init__(self, dm, h):
+        self.dm = dm
+        self.h = h
+        self.depth = dm // h
+        self.Wq = tf.keras.layers.Dense(dm)
+        self.Wk = tf.keras.layers.Dense(dm)
+        self.Wv = tf.keras.layers.Dense(dm)
+        self.linear = tf.keras.layers.Dense(dm)
+
+    def reshape_tensor(self, x, batch):
+        x = tf.reshape(x, (batch, -1, self.h, self.depth))
+        return tf.transpose(x, perm=(0, 2, 1, 3))
+
+    def __call__(self, Q, K, V, mask):
+        batch = Q.shape[0]
+
+        V = self.reshape_tensor(self.Wv(V), batch)
+        Q = self.reshape_tensor(self.Wq(Q), batch)
+        K = self.reshape_tensor(self.Wk(K), batch)
+
+        scaled_attention, weights = sdp_attention(Q, K, V, mask)
+
+        scaled_attention = tf.transpose(scaled_attention, perm=[0, 2, 1, 3])
+        concat_attention = tf.reshape(scaled_attention, (batch, -1, self.dm))
+        output = self.linear(concat_attention)
+
+        return output, weights
