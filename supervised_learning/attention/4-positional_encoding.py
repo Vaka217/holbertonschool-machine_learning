9f914b8dc2d2040283092a5b33342d9cf40819ae
Vaka217 <gonzalocabaleiro12@gmail.com>
2024-01-09 11:56:27 -0300

    Task 4 and 5

---
+++ supervised_learning/attention/4-positional_encoding.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+"""Positional Encoding Module"""
+import numpy as np
+
+
+def positional_encoding(max_seq_len, dm):
+    """Calculates the positional encoding for a transformer:
+
+    max_seq_len is an integer representing the maximum sequence length
+    dm is the model depth
+
+    Returns: a numpy.ndarray of shape (max_seq_len, dm) containing the
+    positional encoding vectors"""
+
+    PE = np.zeros((max_seq_len, dm))
+    for k in range(max_seq_len):
+        for i in np.arange(int(dm / 2)):
+            n_pow = np.math.pow(10000, 2 * i / dm)
+            PE[k, 2 * i] = np.math.sin(k / n_pow)
+            PE[k, 2 * i + 1] = np.math.cos(k / n_pow)
+
+    return PE
---
+++ supervised_learning/attention/5-sdp_attention.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python3
+"""Scaled Dot Product Attention Module"""
+import tensorflow as tf
+
+
+def sdp_attention(Q, K, V, mask=None):
+    """Calculates the scaled dot product attention:
+
+    Q is a tensor with its last two dimensions as (..., seq_len_q, dk)
+    containing the query matrix
+    K is a tensor with its last two dimensions as (..., seq_len_v, dk)
+    containing the key matrix
+    V is a tensor with its last two dimensions as (..., seq_len_v, dv)
+    containing the value matrix
+    mask is a tensor that can be broadcast into (..., seq_len_q, seq_len_v)
+    containing the optional mask, or defaulted to None
+    if mask is not None, multiply -1e9 to the mask and add it to the scaled
+    matrix multiplication
+    The preceding dimensions of Q, K, and V are the same
+
+    Returns: output, weights
+    output a tensor with its last two dimensions as (..., seq_len_q, dv)
+    containing the scaled dot product attention
+    weights a tensor with its last two dimensions as
+    (..., seq_len_q, seq_len_v) containing the attention weights"""
+
+    scores = tf.matmul(Q, K, transpose_b=True) / \
+        tf.sqrt(tf.cast(Q.shape[-1], 'float32'))
+
+    if mask:
+        scores = mask * -1e9
+
+    weights = tf.keras.backend.softmax(scores)
+    output = tf.matmul(weights, V)
+
+    return output, weights
