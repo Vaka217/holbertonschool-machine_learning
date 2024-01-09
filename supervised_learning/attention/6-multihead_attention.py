d8200b6816ae5e4ecc631c40741eacede7b1c8c3
Vaka217 <gonzalocabaleiro12@gmail.com>
2024-01-09 13:28:59 -0300

    Everything documented

--- supervised_learning/attention/6-multihead_attention.py
+++ supervised_learning/attention/6-multihead_attention.py
@@ -5,7 +5,23 @@ sdp_attention = __import__('5-sdp_attention').sdp_attention
 
 
 class MultiHeadAttention(tf.keras.layers.Layer):
-    """"""
+    """Perform multi head attention
+
+    Class constructor def __init__(self, dm, h):
+        dm is an integer representing the dimensionality of the model
+        h is an integer representing the number of heads
+        dm is divisible by h
+        Sets the following public instance attributes:
+        h - the number of heads
+        dm - the dimensionality of the model
+        depth - the depth of each attention head
+        Wq - a Dense layer with dm units, used to generate the query matrix
+        Wk - a Dense layer with dm units, used to generate the key matrix
+        Wv - a Dense layer with dm units, used to generate the value matrix
+        linear - a Dense layer with dm units, used to generate the attention
+        output
+
+    Public instance method def call(self, Q, K, V, mask)"""
 
     def __init__(self, dm, h):
         super(MultiHeadAttention, self).__init__()
@@ -18,10 +34,25 @@ class MultiHeadAttention(tf.keras.layers.Layer):
         self.linear = tf.keras.layers.Dense(dm)
 
     def reshape_tensor(self, x, batch):
+        """Split over the last axis of any given x"""
         x = tf.reshape(x, (batch, -1, self.h, self.depth))
-        return tf.transpose(x, perm=[0, 2, 1, 3])
+        x = tf.transpose(x, perm=[0, 2, 1, 3])
+        return x
 
     def __call__(self, Q, K, V, mask):
+        """Q is a tensor of shape (batch, seq_len_q, dk) containing the input
+        to generate the query matrix
+        K is a tensor of shape (batch, seq_len_v, dk) containing the input to
+        generate the key matrix
+        V is a tensor of shape (batch, seq_len_v, dv) containing the input to
+        generate the value matrix
+        mask is always None
+
+        Returns: output, weights
+        outputa tensor with its last two dimensions as (..., seq_len_q, dm)
+        containing the scaled dot product attention
+        weights a tensor with its last three dimensions as
+        (..., h, seq_len_q, seq_len_v) containing the attention weights"""
         batch = tf.shape(Q)[0]
 
         V = self.Wv(V)
