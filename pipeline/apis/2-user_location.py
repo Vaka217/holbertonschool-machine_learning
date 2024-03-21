6be38331cb3041650309244a8a7a0f765badacd4
Vaka217 <gonzalocabaleiro12@gmail.com>
2024-03-21 12:24:31 -0300

    Task 2 fix 403 case

--- pipeline/apis/2-user_location.py
+++ pipeline/apis/2-user_location.py
@@ -11,20 +11,20 @@ Your code should not be executed when the file is imported (you should use
 if __name__ == '__main__':)"""
 import sys
 import requests
-import datetime
+import time
 
 
 if __name__ == '__main__':
+    if len(sys.argv) != 2:
+        exit()
     user = requests.get(sys.argv[1])
     if user.status_code == 404:
         print('Not found')
     elif user.status_code == 403:
-        reset_time = datetime.datetime.utcfromtimestamp(
-            user.headers['X-Ratelimit-Reset'])
-        current_time = datetime.datetime.now()
-        time_difference =  reset_time - current_time
-        time_difference_minutes = time_difference.total_seconds() / 60
-        print('Reset in {} min'.format(user.headers['X-Ratelimit-Reset']))
+        reset_time = int(user.headers['X-Ratelimit-Reset'])
+        current_time = int(time.time())
+        time_difference = int((reset_time - current_time) / 60)
+        print('Reset in {} min'.format(time_difference))
     else:
         user = user.json()
         print(user['location'])
